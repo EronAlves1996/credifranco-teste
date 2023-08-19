a684666e40e06a53776e686e73ffdcc6c15f11c6
EronAlves1996 <eron.alves@rocketmail.com>
2023-08-19 14:03:27 -0300

    feat: prepare database

--- src/back/database/migrations/2023_08_19_155820_create_users.php
+++ src/back/database/migrations/2023_08_19_155820_create_users.php
@@ -14,10 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('identification')->unique();
+            $table->enum('identification_type', ['cpf', 'gerente']);
+            $table->enum('role', ['MANAGER', "CLIENT"]);
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('accumulated_points');
             $table->timestamps();
         });
     }
