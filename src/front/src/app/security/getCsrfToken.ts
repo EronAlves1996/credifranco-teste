69c72970d773578c7322e2db2e0389d69120890c
EronAlves1996 <eron.alves@rocketmail.com>
2023-08-22 16:00:57 -0300

    Revert "Release 1.0 (#33)" (#34)
    This reverts commit a9cf949c00cd538acba0801e0d64e97016f44183.

--- src/front/src/app/security/getCsrfToken.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "@/config";
-import { storeCsrf } from "./storeCsrf";
-
-export const getCsrfToken = () => {
-  return fetch(config.API_URL + "sanctum/csrf-cookie", {
-    credentials: "include",
-  }).then(() => storeCsrf());
-};
