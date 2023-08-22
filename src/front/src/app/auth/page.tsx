629cdc88beb084f9aacd1079f634e959fe43fbbe
EronAlves1996 <eron.alves@rocketmail.com>
2023-08-22 00:19:33 -0300

    feat: implement a basic login in front end

--- src/front/src/app/auth/page.tsx
+++ src/front/src/app/auth/page.tsx
@@ -1,15 +1,63 @@
+"use client";
 import { DefaultButton } from "@/components/DefaultButton";
+import { config } from "@/config";
 import { Box, FormLabel, Stack, TextField } from "@mui/material";
+import * as authStore from "../authStore";
 
 const formFields = [
-  { label: "Seu ID", type: "text" },
-  { label: "Senha", type: "password" },
+  { label: "Seu ID", type: "text", name: "identification" },
+  { label: "Senha", type: "password", name: "password" },
 ];
 
 export default function Home() {
   return (
     <Stack justifyContent="center">
-      <Box component="form">
+      <Box
+        component="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const { target } = e;
+
+          const formData = new FormData(target as unknown as HTMLFormElement);
+
+          const payload = Object.fromEntries(formData);
+
+          fetch(config.API_URL + "sanctum/csrf-cookie", {
+            credentials: "include",
+          })
+            .then(() => {
+              const token = document.cookie
+                .split("; ")
+                .find((cookie) => cookie.includes("XSRF-TOKEN"))
+                ?.split("=")[1];
+
+              if (token) authStore.put("XSRF_TOKEN", decodeURIComponent(token));
+            })
+            .then(() =>
+              fetch(config.API_URL + "api/login", {
+                body: JSON.stringify(payload),
+                method: "POST",
+                credentials: "include",
+                headers: {
+                  "X-XSRF-TOKEN": authStore.get("XSRF_TOKEN"),
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                } as HeadersInit,
+              })
+            )
+            .then((response) => response.json())
+            .then((user) => {
+              authStore.put("CURRENT_USER", user);
+
+              const token = document.cookie
+                .split("; ")
+                .find((cookie) => cookie.includes("XSRF-TOKEN"))
+                ?.split("=")[1];
+
+              if (token) authStore.put("XSRF_TOKEN", decodeURIComponent(token));
+            });
+        }}
+      >
         <Stack gap="2rem">
           <FormLabel sx={{ fontWeight: "bold", fontSize: "1.2rem" }}>
             Área Exclusiva
@@ -19,10 +67,11 @@ export default function Home() {
               key={i}
               variant="outlined"
               sx={{ backgroundColor: "#FFFFFFBB" }}
+              required
               {...fieldInfo}
             />
           ))}
-          <DefaultButton>Login</DefaultButton>
+          <DefaultButton type="submit">Login</DefaultButton>
         </Stack>
       </Box>
     </Stack>
---
+++ src/front/src/app/authStore.ts
@@ -0,0 +1,21 @@
+type User = {
+  accumulated_points: number;
+  created_at: string;
+  id: number;
+  name: string;
+  role: "MANAGER" | "CLIENT";
+  updated_at: string;
+};
+
+const store: Record<"XSRF_TOKEN" | "CURRENT_USER", string | User> = {
+  XSRF_TOKEN: "",
+  CURRENT_USER: {} as User,
+};
+
+export const put = (key: keyof typeof store, value: string | User) => {
+  store[key] = value;
+};
+
+export const get = (key: keyof typeof store) => {
+  return store[key];
+};
