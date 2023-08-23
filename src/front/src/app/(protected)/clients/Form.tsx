3b8ab77ee8986d3b744f5159de26467c941a5778
EronAlves1996 <eron.alves@rocketmail.com>
2023-08-23 13:18:56 -0300

    feat: create product form realigned

---
+++ src/front/src/app/(protected)/clients/Form.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Box } from "@mui/material";
+import { PropsWithChildren } from "react";
+import * as authStore from "../../authStore";
+import { checkActiveSession, updateUserInfo } from "@/app/utils/fetchUtils";
+import { extractPayloadFromForm } from "@/app/utils/extractPayloadFromForm";
+import { toast } from "react-toastify";
+
+export const Form = ({ children }: PropsWithChildren) => {
+  const notify = (content: string, type: "error" | "success") =>
+    toast(content, { type });
+
+  return (
+    <Box
+      component="form"
+      onSubmit={(e) => {
+        e.preventDefault();
+
+        const { target } = e;
+
+        const formData = extractPayloadFromForm(target);
+
+        const user = structuredClone(authStore.get("CURRENT_USER"));
+
+        if (!authStore.isUser(user)) return;
+
+        const newPoints = user.accumulated_points - Number(formData["points"]);
+
+        if (isNaN(newPoints) || newPoints < 0) {
+          notify(
+            "Pontos para voucher são maiores do que os pontos que você tem",
+            "error"
+          );
+
+          throw new Error("Invalid Points");
+        }
+
+        user.accumulated_points = newPoints;
+
+        updateUserInfo(user.id, user)
+          .then((res) => {
+            if (!res.ok) {
+              notify("Voucher não foi criado", "error");
+              throw new Error("");
+            }
+
+            notify("Seu voucher XXX foi criado com sucesso", "success");
+            // gonna refetch user info
+            return checkActiveSession();
+          })
+          .then((res) => res.json())
+          .then((user) => authStore.put("CURRENT_USER", user));
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
--- src/front/src/app/(protected)/manager/products/page.tsx
+++ src/front/src/app/(protected)/manager/products/page.tsx
@@ -1,37 +1,71 @@
-import { FormLabel, Link, Stack, TextField, Typography } from "@mui/material";
-import { Form } from "./Form";
+"use client";
+import { FormLabel, Stack, TextField, Typography } from "@mui/material";
 import { DefaultButton } from "@/components/DefaultButton";
 import { Metadata } from "next";
+import { ChangeEvent, useState } from "react";
+import { Form } from "./Form";
 
 export const metadata: Metadata = {
   title: "Area do Gerente",
 };
 
-const formFields = [
-  { label: "Produto", type: "text", name: "product" },
-  { label: "Preço", type: "number", name: "price" },
-  { label: "Desconto", type: "number", name: "discount", min: "0", max: "100" },
-];
-
 export default function ManageProducts() {
+  const [discount, setDiscount] = useState(0);
+  const [price, setPrice] = useState(0);
+
+  const formFields = [
+    { label: "Produto", type: "text", name: "product" },
+    {
+      label: "Preço",
+      type: "number",
+      name: "price",
+      value: price,
+      onChange: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+        setPrice(parseFloat(e.target.value)),
+    },
+    {
+      label: "Desconto",
+      type: "number",
+      name: "discount",
+      min: "0",
+      max: "100",
+      value: discount,
+      onChange: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+        setDiscount(parseFloat(e.target.value)),
+    },
+  ];
+
   return (
     <>
       <Stack justifyContent="center">
         <Form>
-          <Stack gap="2rem">
-            <FormLabel sx={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+          <Stack alignItems="stretch" gap="1rem">
+            <FormLabel
+              sx={{
+                fontWeight: "bold",
+                fontSize: "1.2rem",
+                alignSelf: "center",
+              }}
+            >
               Registrar Promoção
             </FormLabel>
-            {formFields.map((fieldInfo, i) => (
-              <TextField
-                key={i}
-                variant="outlined"
-                sx={{ backgroundColor: "#FFFFFFBB" }}
-                required
-                {...fieldInfo}
-              />
-            ))}
-            <DefaultButton type="submit">Registrar Promoção</DefaultButton>
+            <Stack gap="2rem" direction="row" alignSelf="center">
+              {formFields.map((fieldInfo, i) => (
+                <TextField
+                  key={i}
+                  variant="outlined"
+                  sx={{ backgroundColor: "#FFFFFFBB" }}
+                  required
+                  {...fieldInfo}
+                />
+              ))}
+            </Stack>
+            <Stack direction="row" justifyContent="space-around">
+              <Typography>
+                Valor Final: R$ {(price * (1 - discount / 100)).toFixed(2)}
+              </Typography>
+              <DefaultButton type="submit">Registrar Promoção</DefaultButton>
+            </Stack>
           </Stack>
         </Form>
       </Stack>
